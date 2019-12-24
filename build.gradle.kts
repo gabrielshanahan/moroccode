a05ed0be418aa6a6d46b7703ab096a960e75cda6
Gabriel Shanahan <GabrielShanahan@gmail.com>
2019-12-24 15:38:59 +0100

    Initial setup

---
+++ build.gradle.kts
@@ -0,0 +1,63 @@
+plugins {
+    kotlin("jvm") version "1.3.61"
+
+    `java-library`
+    jacoco
+
+    id("org.jmailen.kotlinter") version "2.1.3"
+    id("io.gitlab.arturbosch.detekt").version("1.2.2")
+    id("org.sonarqube") version "2.8"
+    id("org.jetbrains.dokka") version "0.10.0"
+}
+
+repositories {
+    mavenCentral()
+    jcenter()
+}
+
+group = "com.github.gabrielshanahan.moroccode"
+version = "0.0.1a"
+
+dependencies {
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    testImplementation("io.kotlintest:kotlintest-runner-junit5:3.3.2")
+
+    // This is needed due to a known issue with KotlinTest: https://github.com/kotlintest/kotlintest/issues/639
+    testImplementation("org.slf4j", "slf4j-simple", "1.7.26")
+
+    // Adds detekt wrapper around ktlint
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.0.1")
+}
+
+tasks {
+    test {
+        useJUnitPlatform()
+        // Always run detekt (and through the klint wrapper, klint) and jacoco when running tests
+        finalizedBy(detekt)
+        finalizedBy(jacocoTestReport)
+
+        testLogging {
+            // Make sure output from
+            // standard out or error is shown
+            // in Gradle output.
+            showStandardStreams = true
+        }
+    }
+
+    dokka {
+        outputFormat = "html"
+        outputDirectory = "$buildDir/javadoc"
+        configuration {
+            includeNonPublic = true
+            reportUndocumented = true
+        }
+    }
+}
+
+detekt {
+    toolVersion = "1.2.2"
+    parallel = true // Builds the AST in parallel. Rules are always executed in parallel.
+}
+
