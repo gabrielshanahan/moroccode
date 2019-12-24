13888c37249b85e1196ae0b6764cac78acb7ad04
Gabriel Shanahan <GabrielShanahan@gmail.com>
2019-12-24 16:14:16 +0100

    Implemented hash as alias for builtin functions

---
+++ src/main/kotlin/moroccode/Hash.kt
@@ -0,0 +1,39 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017 Pim van den Berg
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package moroccode
+
+/**
+ * Alias for [java.util.Objects.hashCode]. Defined to prevent unnecessary wrapping in an array.
+ *
+ * @param field The field to generate hash code from
+ */
+fun hash(field: Any?) = java.util.Objects.hashCode(field)
+
+/**
+ * Alias for [contentHashCode].
+ *
+ * @param field The fields to generate hash code from
+ */
+fun hash(vararg fields: Any?) = fields.contentHashCode()
